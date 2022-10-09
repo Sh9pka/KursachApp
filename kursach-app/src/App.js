d4874b6aca466d11d05c319851ef0f43dd425ba9
Sh9pka <sh9pka@gmail.com>
2022-10-09 17:51:41 +0300

    header

--- kursach-app/src/App.js
+++ kursach-app/src/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import HeaderApp from './components/header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className='header'>
+        <HeaderApp/>
       </header>
-      <div>qwe</div>
     </div>
   );
 }
--- kursach-app/src/components/header.js
+++ kursach-app/src/components/header.js
@@ -1,3 +1,23 @@
-function HeaderApp () {
+import './header.css'
 
+function HeaderApp () {
+  return (
+    <div className="nav-header">
+      <div>
+        <a href="#" className="nav-button">Name</a>
+      </div>
+      <div>
+        <ul className="nav-items">
+          <li><a href="#" className="nav-button">Button 1</a></li>
+          <li><a href="#" className="nav-button">Button 2</a></li>
+          <li><a href="#" className="nav-button">Button 3</a></li>
+        </ul>
+      </div>
+      <div>
+        <a href="#" className="nav-button">Button</a>
+      </div>
+    </div>
+  )
 }
+
+export default HeaderApp;
