9adcc96ddea19d9c2be98cb409c9207877931d3b
Sh9pka <sh9pka@gmail.com>
2022-10-11 18:27:07 +0300

    A lot of shit

--- kursach-app/src/App.js
+++ kursach-app/src/App.js
@@ -1,13 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-import HeaderApp from './components/header';
+import HeaderApp from './components/header/header';
+import FooterApp from './components/footer/footer';
+import SchemeApp from './components/main/workspace/scheme';
+import AppliancesApp from './components/main/workspace/appliances';
 
 function App() {
   return (
     <div className="App">
-      <header className='header'>
+      <header className="header">
         <HeaderApp/>
       </header>
+      <main className="workspace">
+        <SchemeApp/>
+        <AppliancesApp/>
+      </main>
+      <footer className="footer">
+        <FooterApp/>
+      </footer>
     </div>
   );
 }
---
+++ kursach-app/src/components/footer/footer.js
@@ -0,0 +1,11 @@
+import './footer.css'
+
+function FooterApp () {
+  return (
+    <div className="nav-footer">
+      <h1>Футер</h1>
+    </div>
+  )
+}
+
+export default FooterApp;
